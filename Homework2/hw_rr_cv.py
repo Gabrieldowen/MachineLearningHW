e3c1d20095ec9a6a50d02658f34ab40dde6e59f0
Gabriel <Gabriel.d.owen-1@ou.edu>
2023-09-15 19:37:30 -0500

    changed Cross-Validation to only portion the training set into k parts

--- Homework2/hw_rr_cv.py
+++ Homework2/hw_rr_cv.py
@@ -9,35 +9,35 @@ data = np.loadtxt('crime.csv', delimiter=',', skiprows=1)
 
 def RR(tau, CustomLambda, K):
 
+
+    n = len(data)
+
+    # Split the data and set aside testing data
+    num_train = int(tau * n)
+    num_test = n - num_train
+
+    # Test data copy
+    sample_test = data[num_train:, :-1]
+    label_test = data[num_train:, -1]
+
+    fold_size = len(data[:num_train, :-1]) // K
+
     K_Errors = []
     for i in range(K):
-        print(f"Fold {i + 1}:")
+        # print(f"Fold {i + 1}:")
 
         # Determine the indices for the current fold
-        fold_size = len(data) // K
         start_idx = i * fold_size
         end_idx = (i + 1) * fold_size
 
+        # print(f"start {start_idx} end {end_idx}")
 
         # Split the data
-        num_train = int(tau * fold_size)
-        num_test = fold_size - num_train
+        num_train = int(fold_size)
 
         # Training Data copy within the current fold
-        sample_train = data[start_idx:(end_idx-num_test), :-1]
-        label_train = data[start_idx:(end_idx-num_test), -1]
-
-        # Test data copy within the current fold
-        sample_test = data[(start_idx+num_train):end_idx, :-1]
-        label_test = data[(start_idx+num_train):end_idx, -1]
-
-        # Training Data copy
-        #sample_train = kportion[:num_train, :-1]
-        #label_train = kportion[:num_train, -1]
-
-        # Test data copy
-        #sample_test = kportion[num_train:, :-1]
-        #label_test = kportion[num_train:, -1]
+        sample_train = data[start_idx:end_idx, :-1]
+        label_train = data[start_idx:end_idx, -1]
 
         # Variables for calculation
         X = np.hstack((sample_train, np.ones((num_train, 1))))  # Add a column of ones for the intercept
@@ -50,7 +50,6 @@ def RR(tau, CustomLambda, K):
         # ACTUAL TRAINING
         beta = np.linalg.inv(X_transpose @ X + RidgePenalty) @ X_transpose @ label_train
 
-
         # Calculate predictions
         sample_train_with_intercept = np.hstack((sample_train, np.ones((num_train, 1))))
         train_prediction = sample_train_with_intercept @ beta
@@ -61,17 +60,19 @@ def RR(tau, CustomLambda, K):
 
         # Calculate the Test error with weighted squared differences and print
         error_test = mean_squared_error(label_test, test_prediction)
-        print(f"Test Error: {error_test}")
-        K_Errors.append(error_test)
+        error_train = mean_squared_error(label_train, train_prediction)
+
+        #print(f"Test Error: {error_test}")
+        K_Errors.append(train_prediction)
 
     return K_Errors
 
 
 # Pick 5 values of τ, including τ = 1
 tau = 0.75
-lam = 0.6
+lam = [0.01, 0.1, 0.25, 0.5, 0.9]
 K = 10
 
-K_Errors = RR(tau, lam, K)
-
-print(f"K_Error average:{np.mean(K_Errors)}")
+for L in lam:
+    K_Errors = RR(tau, L, K)
+    print(f"Validation Error (Lam={L}):{np.mean(K_Errors)}")
