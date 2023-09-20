44405de53562e28d81b97b9beaaf30403c8132ff
Gabriel <Gabriel.d.owen-1@ou.edu>
2023-09-20 09:57:14 -0500

    INIT Homework3

---
+++ HomeWork3/LassoExample.py
@@ -0,0 +1,35 @@
+import numpy as np
+from sklearn import linear_model
+from sklearn.metrics import mean_squared_error
+
+# load data
+data = np.loadtxt('/Users/gabeowen/Documents/ML_HW/Data/crime.csv', delimiter=',', skiprows=1)
+[n,p] = np.shape(data)
+
+# set training number and testing number
+num_train = int(0.75*n)
+num_test = int(0.25*n)
+
+# split data into training set and testing set
+sample_train = data[0:num_train,0:-1]
+sample_test = data[n-num_test:,0:-1]
+label_train = data[0:num_train,-1]
+label_test = data[n-num_test:,-1]
+
+# hyper-parameter
+lamda = 0.1
+# train model and use it to make prediction
+model = linear_model.Lasso(lamda)
+model.fit(sample_train, label_train)
+label_train_pred = model.predict(sample_train)
+label_test_pred = model.predict(sample_test)
+
+# evaluate model error
+error_train = mean_squared_error(label_train, label_train_pred)
+error_test = mean_squared_error(label_test, label_test_pred)
+
+# print results or draw a figure
+print("Training Error = %.4f" % error_train)
+print('Testing Error = %.4f' % error_test)
+
+# model.coef_
